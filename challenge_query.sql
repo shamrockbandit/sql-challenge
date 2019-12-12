8bea161c45f4951d0a8d1ac6ca9767460d6d4aa7
shamrockbandit <sgbarnett@wustl.edu>
2019-12-11 21:17:03 -0600

    still committing

--- challenge_query.sql
+++ challenge_query.sql
@@ -7,3 +7,10 @@ inner join salaries on employees.emp_no=salaries.emp_no;
 select * 
 from employees
 where hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--find info for the department managers
+select dept_manager.dept_no, departments.dept_name, employees.first_name, employees.last_name, dept_manager.emp_no, dept_manager.from_date, dept_manager.to_date, salaries.salary  
+from dept_manager
+inner join salaries on dept_manager.emp_no=salaries.emp_no
+inner join departments on dept_manager.dept_no=departments.dept_no
+inner join employees on dept_manager.emp_no=employees.emp_no;
