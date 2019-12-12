fddd3c5e9b22163c9b356c396f72920f872a506c
shamrockbandit <sgbarnett@wustl.edu>
2019-12-11 20:46:44 -0600

    pushing yet another commit

--- challenge_query.sql
+++ challenge_query.sql
@@ -2,3 +2,8 @@
 select employees.first_name, employees.last_name, employees.gender, salaries.salary 
 from employees
 inner join salaries on employees.emp_no=salaries.emp_no;
+
+--find employees hired in 1986
+select * 
+from employees
+where hire_date BETWEEN '1986-01-01' AND '1986-12-31';
